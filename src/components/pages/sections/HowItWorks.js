dd1ac548b03dd454dc5435c3b4441a5d609c51d9
MaximGromak <maximgromak@gmail.com>
2022-06-13 11:47:15 +1000

    add: how it works

--- src/components/pages/sections/HowItWorks.js
+++ src/components/pages/sections/HowItWorks.js
@@ -1,3 +1,19 @@
-export default function HowItWorks(props){
-    return null;
+import React from "react";
+
+export default function HowItWorks(props) {
+    return (
+        <section className="features-section" id="features">
+            <div className="container">
+                <div className="features-top">
+                    <h2 className="features-title">Как это работает</h2>
+                    <button type="button" className="features-btn">Узнать больше</button>
+                </div>
+                <div className="features-list">
+                    <div className="features-item features-order">Удобный заказ на сайте</div>
+                    <div className="features-item features-office">Нет необходимости ехать в офис</div>
+                    <div className="features-item features-world">Огромный выбор направлений</div>
+                </div>
+            </div>
+        </section>
+    );
 }
