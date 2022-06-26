6f2934fc299484281e2279663d149a1cbd28e9bf
MaximGromak <maximgromak@gmail.com>
2022-06-26 17:28:54 +1000

    add progressbar

--- src/components/pages/sections/findTickets/FindTicketWidget.js
+++ src/components/pages/sections/findTickets/FindTicketWidget.js
@@ -1,32 +1,36 @@
 import React from "react";
+import Progressbar from "./progress/Progressbar";
 
 export default function FindTicketWidget(props) {
 
     return (
-        <section className='widget-search-wrapper'>
-            <div className='motto'>
-                <span>Вся жизнь - </span>
-                <span> путешествие!</span>
-            </div>
-            <form className='widget-search-ticket'>
-                <div className='direction'>
-                    <h3>Направление</h3>
-                    <div>
-                        <input className='from' type='text' placeholder='Откуда'/>
-                        <div className='pic-refresh'/>
-                        <input className='to' type='text' placeholder='Куда'/>
-                    </div>
+        <>
+            <Progressbar/>
+            <section className='widget-search-wrapper'>
+                <div className='motto'>
+                    <span>Вся жизнь - </span>
+                    <span> путешествие!</span>
                 </div>
-                <div className='date'>
-                    <h3>Дата</h3>
-                    <div>
-                        <input className='from' type='date' placeholder='ДД/ММ/ГГ'/>
-                        <div className='pic-refresh'/>
-                        <input className='to' type='text' placeholder='ДД/ММ/ГГ'/>
+                <form className='widget-search-ticket'>
+                    <div className='direction'>
+                        <h3>Направление</h3>
+                        <div>
+                            <input className='from' type='text' placeholder='Откуда'/>
+                            <div className='pic-refresh'/>
+                            <input className='to' type='text' placeholder='Куда'/>
+                        </div>
                     </div>
-                </div>
-                <button type="submit" className="form-search-btn">Найти билет</button>
-            </form>
-        </section>
+                    <div className='date'>
+                        <h3>Дата</h3>
+                        <div>
+                            <input className='from' type='date' placeholder='ДД/ММ/ГГ'/>
+                            <div className='pic-refresh'/>
+                            <input className='to' type='text' placeholder='ДД/ММ/ГГ'/>
+                        </div>
+                    </div>
+                    <button type="submit" className="form-search-btn">Найти билет</button>
+                </form>
+            </section>
+        </>
     );
 }
---
+++ src/components/pages/sections/findTickets/progress/Progressbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function Progressbar({isPassangerPage, isPayment, isVerification}) {
+    return (
+        <div className='progressbar'>
+            <div className='stage'>
+                <div className='number'>
+                    <span>1</span>
+                </div>
+                <span className='stage-title'>Билеты</span>
+                <div className='stage-arrow'/>
+            </div>
+            <div className='stage'>
+                <div className='number'>
+                    <span>2</span>
+                </div>
+                <span className='stage-title'>Пассажиры</span>
+                <div className='stage-arrow'/>
+            </div>
+            <div className='stage'>
+                <div className='number'>
+                    <span>3</span>
+                </div>
+                <span className='stage-title'>Оплата</span>
+                <div className='stage-arrow'/>
+            </div>
+            <div className='stage'>
+                <div className='number'>
+                    <span>4</span>
+                </div>
+                <span className='stage-title'>Проверка</span>
+            </div>
+
+        </div>
+    )
+}
