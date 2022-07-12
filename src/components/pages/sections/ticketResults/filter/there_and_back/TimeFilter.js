97d5da247cf4ea4a0e0bad8cf7a14cdf7e8fbf4d
MaximGromak <maximgromak@gmail.com>
2022-07-13 02:32:30 +1000

    time sliders interaction

--- src/components/pages/sections/ticketResults/filter/there_and_back/TimeFilter.js
+++ src/components/pages/sections/ticketResults/filter/there_and_back/TimeFilter.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import TimeSlider from "./TimeSlider";
 
-export default function TimeFilter(props){
-    return (
-        <div className="time-filter">
-            <div className="time-filter-top">
-                <div className="time-filter-title">
-                    <div className="time-icon">
-                        <img src={props.icon} alt="icon arrow-right"/>
-                    </div>
-                    <div className="time-name">{props.title}</div>
-                </div>
-                <div className="time-filter-btn"/>
-            </div>
+export default function TimeFilter(props) {
+    const [opened, setOpened] = useState(false);
 
-            <TimeSlider title='Время отбытия' align='left' />
-            <TimeSlider title='Время прибытия' align='right' />
+    function handler(){
+        console.log('clicked')
+        if (opened) setOpened(false);
+        if (!opened) setOpened(true);
+    }
+
+    const commonPart = <div className="time-filter-top">
+        <div className="time-filter-title">
+            <div className="time-icon">
+                <img src={props.icon} alt="icon arrow-right"/>
+            </div>
+            <div className="time-name">{props.title}</div>
         </div>
-    )
+        <div className="time-filter-btn" onClick={handler}/>
+    </div>;
+
+    if (opened){
+        return (
+            <div className="time-filter">
+                {commonPart}
+
+                <TimeSlider title='Время отбытия' align='left'/>
+                <TimeSlider title='Время прибытия' align='right'/>
+            </div>
+        )
+    } else {
+        return (
+            <div className="time-filter">
+                {commonPart}
+            </div>
+        )
+    }
+
+
 }
