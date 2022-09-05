61ac2335c2c1267f904bdf1da82ff429d1effb4c
MaximGromak <maximgromak@gmail.com>
2022-09-06 04:03:15 +1000

    remove useless code

--- src/components/pages/1_main_page/MainPage.js
+++ src/components/pages/1_main_page/MainPage.js
@@ -1,5 +1,4 @@
 import React, {useState} from "react";
-import UserPaymentData from "../5_payment_page/dataObject/UserPaymentData";
 
 import './css/mainPage.css';
 import MiddleSection from "./sections/MiddleSection";
@@ -63,77 +62,11 @@ export default function MainPage(props) {
     )
 
 
-    const [display, setDisplay] = useState(); // отображаемая в данный момент middle секция
-    const [filter, setFilter] = useState(); // фильтры
-    const [routes, setRoutes] = useState(null); // направления
-
-    const [seatsData, setSeatsData] = useState();
-    const [selectedTrain, setSelectedTrain] = useState();
-    const [selectedWagon, setSelectedWagon] = useState();
-    const [selectedSeatsData, setSelectedSeats] = useState(new Set());
-    const [selectedPassengersData, setPassengers] = useState(new Set());
-    const [paymentData, setPaymentData] = useState(new UserPaymentData());
-
-
     let classes = null;
-
-    function openSeats() {
-        setDisplay('seats');
-    }
-
-    function openOffers() {
-        setDisplay('offers');
-    }
-
-    function openPassengers() {
-        setDisplay('passengers');
-    }
-
-    function openPayment() {
-        if (selectedPassengersData.size !== 0 && selectedSeatsData.size !== 0) {
-            if (selectedPassengersData.size === selectedSeatsData.size) {
-                setDisplay('payment');
-            }
-        }
-    }
-
-    function openConfirm() {
-        setDisplay('confirm');
-    }
-
-    function openThnx() {
-        classes = 'main-page bgr3';
-        setDisplay('thnx');
-    }
-
-    function selectAnyWagon(index) {
-        setSelectedWagon(seatsData[index - 1]);
-        console.log('wagon: ' + seatsData);
-    }
-
-    function selectSeats(arr) {
-        setSelectedSeats(arr);
-        console.log('seats:');
-        console.log(selectedSeatsData)
-    }
-
-    //
-    // async function searchSeats(id) {
-    //     let found = offersData.items.filter(offer => offer.departure._id === id)[0];
-    //     setSelectedTrain(found);
-    //
-    //     let result = await apiClient.getSeats(id);
-    //     console.log(result)
-    //     await setSeatsData(result);
-    //     await setSelectedWagon(result[0]);
-    //
-    // }
-
-
-    if (display === 'default') {
-        classes = 'main-page bgr2';
-    } else {
+    if (state.display === 'default') {
         classes = 'main-page bgr1';
+    } else {
+        classes = 'main-page bgr2';
     }
 
     return (
