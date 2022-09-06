a521055d5bd51d6420e0273fbe3f3eb092e642a3
MaximGromak <maximgromak@gmail.com>
2022-09-06 20:05:50 +1000

    add pagination item

--- src/components/pages/2_routes_section/sections/partition/Pagination.js
+++ src/components/pages/2_routes_section/sections/partition/Pagination.js
@@ -2,7 +2,12 @@ import './css/style.css';
 
 import arrow_l_pic from './img/left-chevron.png';
 import arrow_r_pic from './img/right-chevron.png';
+import PaginationItem from "./PaginationItem";
 
+/**
+ * pagination block
+ * (routes section)
+ */
 export default function Pagination(props) {
     return (
         <div className='routes_pagination'>
@@ -10,9 +15,9 @@ export default function Pagination(props) {
                 <img className='pagination_left pagination_arrow' src={arrow_l_pic} alt='pagination left arrow'></img>
             </div>
 
-            <div className='pagination_item active'>1</div>
-            <div className='pagination_item'>2</div>
-            <div className='pagination_item'>3</div>
+            <PaginationItem number='1' isActive={true}/>
+            <PaginationItem number='2' isActive={false}/>
+            <PaginationItem number='3' isActive={false}/>
 
             <div className='pagination_item'>
                 <img className='pagination_right pagination_arrow' src={arrow_r_pic} alt='pagination right arrow'></img>
--- src/components/pages/2_routes_section/sections/partition/PaginationItem.js
+++ src/components/pages/2_routes_section/sections/partition/PaginationItem.js
@@ -1,3 +1,13 @@
+/**
+ * pagination element
+ * (routes section)
+ */
 export default function PaginationItem(props){
-    return null;
+    let classes = 'pagination_item';
+    if (props.isActive){
+        classes = 'pagination_item active';
+    }
+
+    let showThis = <div className={classes}>{props.number}</div>;
+    return showThis;
 }
